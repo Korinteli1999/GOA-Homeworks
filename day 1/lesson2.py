7760b0e08a186f9822739bf81eab7c6f6fe5800a
Korinteli1999 <nika.korinteli05@gmail.com>
2024-10-13 23:10:04 +0400

    second commit

---
+++ day 1/lesson2.py
@@ -0,0 +1,32 @@
+name = "nika"
+#name არის ცვლადი
+# = არის ცვლადისთვის მნიშვნელობის მიმნიჭებელი სიმბოლო
+#"nika" არის ცვლადისთვის მნიშვნელობა 
+surname = ("korinteli")
+
+#print(name)
+#პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+name ="nika"  #ეს არის str (string) ტიპის ცვლაფი
+age = 17 #ეს არის int (integer) მთელი რიცხვი
+height = 182.2 #ეს არის float ტიპის ცვლადი (ათწილადი)
+#boolean (bool) ტიპის ცვლადი 
+
+knows_programming = True #True ან False
+is_ugly = False #snarecase (უბრალოდ წერის სტილი სტანდარტულად)
+
+isUgly = False #ჯავასკრიპტული camelcase
+
+#print(name + " " + surname)
+
+#სტრინგი არის ბრჭყალებში მოქცეულისიმბოლოები.
+#print(name + age)
+
+#print(type(age))
+#print(type(name))
+#print(type(surname))
+#print(type(height))
+#print(type(knows_programming))
+
+print(name + " " + str(age))
+
--- day 2/lesson2.py
+++
@@ -1,6 +0,0 @@
-name = "nika"
-
-surname = ("korinteli")
-
-print(name)
-
