8537e2e26138639ac694be54e46014fa5e31b29f
Korinteli1999 <nika.korinteli05@gmail.com>
2024-10-28 21:33:49 +0400

    commit

---
+++ day 07/homework/hw1.py
@@ -0,0 +1,68 @@
+ #დაწერეთ განსხვავება 
+ #web და back end დეველოპერებს შორის და ვინ არის fullstack დეველოპერი--> <head> </head> <body>
+    #<h1>web dev. vs back-end dev.</h1>
+     #<p>web developer writes the whole site, parts that
+        #people can and can not see, 
+        
+        #while back-end developer writes only part 
+        #which people can not see.
+      
+        #full stack developer is person who can be 
+        #classified as front-end and back-end developer at the same time</p> </body>
+
+
+#<!--3) დაწერეთ რა არის html რაში ვიყენებთ და როგორ ვწერთ html კოდს--> <head> </head>
+
+#<body>  <h1>HTML</h1>
+ #<p> we use HTML to start building website,
+       # and we write it with tags,
+        #what to display and what to not  </p> </body>
+
+
+
+#<!--4) შექმენით ვებსაიტი სადაც გსმოიყენებთ ტექსტს და დაწერთ გოას  შესახებ საიტზე გამოიყენეთ ღილაკი და დააწერეთ Join Us--> <head> </head>
+
+#<body>
+    #<h1>Goa</h1>
+    #<p>გოა ითარგმნება როგორც goal orianted academy და ეს არის აკადემია რომელიც მორგებულია დღევანდელი სწავლების ფორმებს და მოწყვეტილია სხვა მონოტონური და ერთნაირი სასწავლებლების ჯგუფს</p>
+
+    #<h2>n.kesheleva</h2>
+
+    #<p>საქართველოში ყველაზე სანდო და ერთადერთი აკადემია, რომელიც პერსონალურად მოგიდგება და დაგეხმარება ნებისმიერი ცხოვრებისეული პრობლემის მოგვარებაში. დაგისახავ დიად მიზნებს და ბოლომდე მოგეხმარები მათ მიღწევაშიპატივისცემით, ნიკა კეშელავა</p>
+
+    #<button>JOIN GOA</button> </body>
+
+
+
+#<!--5) დაწწრეთ თითოეულის html css და JavaScript ფუნქციები რომელი რაში გამოიყენება--> <head> </head> <body>
+   # <h1>HTML,CSS,JS</h1>   <p>html is the starting point of building a proper website,
+   #      it gives all the neccesities to web site which then are
+    #      reworked by css and finnaly it has it's final form 
+    #      when you give everything their own purpose by js </p> </body>
+
+
+
+#<!--6) რა არის და რისგან შედგება container და empty თეგები-->
+
+#<p>container tags are tags that needs to be opend and closed, empty tags are ones that only need to be opened</p>
+
+
+
+#1) შექნენით 2 ცვლადი თქვენი
+# სახელი და გვარი მოახდინეთ კონკატენაცია
+
+name= "Nika"
+
+surname = "korinteli"
+
+print("my full name is: " +name +" "+surname)
+
+
+
+#2) შექმენით სამი ინტეჯერ ტიპის ცვლადი და დაბეჭდეთ მათი ჯამი
+
+a=22
+b=16784
+c=700
+
+print(a+b+c)
